4d08fad1ca97401d8d7aae747600dca350eb87bb
Suraikin Vladislav <Ghost845JB@gmail.com>
2023-10-07 05:12:24 +0300

    pr3

--- pr3.sql
+++ pr3.sql
@@ -111,11 +111,12 @@
 SELECT DATE_PART('isodow', time)::INTEGER AS weekday_number, TO_CHAR(time, 'Dy') AS weekday,
     COUNT(order_id) FILTER (WHERE action = 'create_order') AS created_orders,
     COUNT(order_id) FILTER (WHERE action = 'cancel_order') AS canceled_orders,
-    COUNT(DISTINCT order_id) - COUNT(order_id) FILTER (WHERE action = 'cancel_order') AS actual_orders,
-    ROUND((COUNT(DISTINCT order_id) - COUNT(order_id) FILTER (WHERE action = 'cancel_order')) / 
-    COUNT(DISTINCT order_id)::DECIMAL, 3) AS success_rate
+    COUNT(order_id) FILTER (WHERE action = 'create_order') - 
+    COUNT(order_id) FILTER (WHERE action = 'cancel_order') AS actual_orders,
+    ROUND((COUNT(order_id) FILTER (WHERE action = 'create_order') -
+    COUNT(order_id) FILTER (WHERE action = 'cancel_order')) / 
+    COUNT(order_id) FILTER (WHERE action = 'create_order')::DECIMAL, 3) AS success_rate
 FROM user_actions
 WHERE time BETWEEN '2022-08-24' AND '2022-09-07'
 GROUP BY 1, 2
 ORDER BY 1
--- Доработать строчки 114-116
